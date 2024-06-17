6cbdce0025d88150b6fd6727d4043bb2c13a5a06
Sonal <sonal.dhage26@gmail.com>
2024-06-17 20:30:11 +0200

    Updated the dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,87 +72,62 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
 
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 
   //  implementation ("androidx.appcompat:appcompat:$rootProject.appCompatVersion")
 
     //Coroutines
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
-    api ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$rootProject.coroutines")
-    api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$rootProject.coroutines")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.20")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
 
     // UI
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation ("com.google.android.material:material:$rootProject.materialVersion")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.android.material:material:1.12.0")
 
     /* coroutines support for firebase operations */
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.1.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
 
     // Import the BoM for the Firebase platform
     implementation (platform("com.google.firebase:firebase-bom:26.1.0"))
     implementation ("com.google.firebase:firebase-firestore")
     implementation ("com.google.firebase:firebase-auth-ktx")
-    implementation ("com.firebaseui:firebase-ui-firestore:6.4.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
 
-    implementation ("com.google.android.gms:play-services-auth:19.0.0")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
 
     implementation ("com.github.bumptech.glide:glide:4.11.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
 
+
+
+    // Import the Firebase BoM
+//    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+//
+//    // When using the BoM, you don't specify versions in Firebase library dependencies
+//
+//    // Add the dependency for the Firebase SDK for Google Analytics
+//    implementation("com.google.firebase:firebase-analytics")
+//
+//    // TODO: Add the dependencies for any other Firebase products you want to use
+//    // See https://firebase.google.com/docs/android/setup#available-libraries
+//    // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+//    implementation("com.google.firebase:firebase-auth")
+//    implementation("com.google.firebase:firebase-firestore")
+
+
+
+
     // Testing
-    testImplementation ("junit:junit:$rootProject.junitVersion")
-    androidTestImplementation ("androidx.arch.core:core-testing:$rootProject.coreTestingVersion")
+    testImplementation ("junit:junit:4.12")
+    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
 //    androidTestImplementation ("androidx.test.espresso:espresso-core:$rootProject.espressoVersion", {
 //        exclude group: 'com.android.support', module: 'support-annotations'
 //    })
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    androidTestImplementation ("androidx.test.ext:junit:$rootProject.androidxJunitVersion")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.1")
 
 }
-//plugins {
-//    id("com.android.application")
-//    id("kotlin-android")
-//    id ("com.google.gms.google-services") // Add this if using Google services
-//    id ("kotlin-kapt") // Adding kotlin-kapt plugin
-//}
-//
-//android {
-//    compileSdk = 34
-//    defaultConfig {
-//        applicationId = "com.example.socialapp"
-//        minSdk = 21
-//        targetSdk = 34
-//        versionCode = 1
-//        versionName = "1.0"
-//        testInstrumentationRunner=  "androidx.test.runner.AndroidJUnitRunner"
-//
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles (
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//
-//}
-//
-//dependencies {
-//    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
-//    implementation ("androidx.core:core-ktx:1.10.1")
-//    implementation ("androidx.appcompat:appcompat:1.6.1")
-//    implementation ("com.google.android.material:material:1.9.0")
-//    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-//    implementation ("com.google.android.gms:play-services-auth:20.5.0")
-//    implementation ("com.google.firebase:firebase-auth-ktx:22.1.1")
-//    implementation ("com.google.firebase:firebase-firestore-ktx:24.3.2")
-//    testImplementation ("junit:junit:4.13.2")
-//    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-//}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,91 @@
+//// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//plugins {
+//    alias(libs.plugins.android.application) apply false
+//    alias(libs.plugins.jetbrains.kotlin.android) apply false
+//    alias(libs.plugins.google.gms.google.services) apply false
+//}
+//
+//buildscript {
+//    val kotlin_version = "1.3.72" // Example value, replace with your actual Kotlin version
+//
+//    repositories {
+//        google()
+//        jcenter()
+//    }
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:4.0.0")
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+//        classpath("com.google.gms:google-services:4.2.0")
+//    }
+//
+//    val appCompatVersion = "1.2.0"
+//    val constraintLayoutVersion = "2.0.2"
+//    val coreTestingVersion = "2.1.0"
+//    val coroutines = "1.3.9"
+//    val lifecycleVersion = "2.2.0"
+//    materialVersion = "1.3.0"
+//    // testing
+//    val junitVersion = "4.13.1"
+//    val espressoVersion = "3.1.0"
+//    val androidxJunitVersion = "1.1.2"
+//}
+//
+//val clean by tasks.registering(Delete::class) {
+//    delete(rootProject.buildDir)
+//}
+//
+//
+
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.services) apply false
 }
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlin_version = "1.3.72" // Example value, replace with your actual Kotlin version
-
+    val kotlin_version = "1.3.72"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("com.google.gms:google-services:4.2.0")
+        classpath ("com.android.tools.build:gradle:4.0.0")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath ("com.google.gms:google-services:4.2.0")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
     }
 
-    val appCompatVersion = "1.2.0"
-    val constraintLayoutVersion = "2.0.2"
-    val coreTestingVersion = "2.1.0"
-    val coroutines = "1.3.9"
-    val lifecycleVersion = "2.2.0"
-    val materialVersion = "1.2.1"
-    // testing
-    val junitVersion = "4.13.1"
-    val espressoVersion = "3.1.0"
-    val androidxJunitVersion = "1.1.2"
+    repositories {
+        google()
+        jcenter()
+    }
 }
 
-val clean by tasks.registering(Delete::class) {
+//allprojects {
+//    repositories {
+//        google()
+//        jcenter()
+//    }
+//}
+
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
-
+ext {
+    set("appCompatVersion", "1.2.0")
+    set("constraintLayoutVersion", "2.0.2")
+    set("coreTestingVersion", "2.1.0")
+    set("coroutines", "1.3.9")
+    set("lifecycleVersion", "2.2.0")
+    set("materialVersion", "1.2.1")
+    // testing
+    set("junitVersion", "4.13.1")
+    set("espressoVersion", "3.1.0")
+    set("androidxJunitVersion", "1.1.2")
+}
