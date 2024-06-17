af3a0eef682dca07f495cdddef927fdbab565f2c
Sonal <sonal.dhage26@gmail.com>
2024-06-17 19:03:19 +0200

    Created Daos and models package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.socialapp"
+    namespace = "com.example.myapp"
     compileSdk = 34
 
     defaultConfig {
@@ -111,3 +112,47 @@ dependencies {
     androidTestImplementation ("androidx.test.ext:junit:$rootProject.androidxJunitVersion")
 
 }
+//plugins {
+//    id("com.android.application")
+//    id("kotlin-android")
+//    id ("com.google.gms.google-services") // Add this if using Google services
+//    id ("kotlin-kapt") // Adding kotlin-kapt plugin
+//}
+//
+//android {
+//    compileSdk = 34
+//    defaultConfig {
+//        applicationId = "com.example.socialapp"
+//        minSdk = 21
+//        targetSdk = 34
+//        versionCode = 1
+//        versionName = "1.0"
+//        testInstrumentationRunner=  "androidx.test.runner.AndroidJUnitRunner"
+//
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles (
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//
+//}
+//
+//dependencies {
+//    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
+//    implementation ("androidx.core:core-ktx:1.10.1")
+//    implementation ("androidx.appcompat:appcompat:1.6.1")
+//    implementation ("com.google.android.material:material:1.9.0")
+//    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation ("com.google.android.gms:play-services-auth:20.5.0")
+//    implementation ("com.google.firebase:firebase-auth-ktx:22.1.1")
+//    implementation ("com.google.firebase:firebase-firestore-ktx:24.3.2")
+//    testImplementation ("junit:junit:4.13.2")
+//    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+//}
--- app/src/main/java/com/example/socialapp/SignInActivity.kt
+++ app/src/main/java/com/example/socialapp/SignInActivity.kt
@@ -1,12 +1,14 @@
 package com.example.socialapp
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp.R
+import com.example.socialapp.daos.UserDao
+import com.example.socialapp.models.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -15,15 +17,14 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.Task
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.firestore.auth.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+
 
 class SignInActivity : AppCompatActivity() {
 
